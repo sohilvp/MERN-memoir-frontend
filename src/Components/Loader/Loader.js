51bf18c2127577fd297e9b2af82a301cc30911c5
sohilvp <128142808+sohilvp@users.noreply.github.com>
2023-11-27 21:38:36 +0530

    Updated

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -1,13 +1,36 @@
-import React from "react";
-import { Ring } from "react-cssfx-loading";
+import React, { useEffect, useState } from "react";
+import { Spin } from "react-cssfx-loading";
 import "./loader.css";
 
 const Loader = () => {
+  const [loading, setLoading] = useState('Loading...')
+  useEffect(()=>{
+    const timeOut1 =  setTimeout(()=>{
+      setLoading('Data is on its way, sit tight')
+     },15000)
+     const timeOut2 =  setTimeout(()=>{
+    setLoading('Almost there...')
+   },29000)
+  
+  const timeOut3 = setTimeout(()=>{
+    setLoading("It's taking longer than expected")
+   },40000)
+
+   return ()=>{
+    clearTimeout(timeOut1)
+    clearTimeout(timeOut2)
+    clearTimeout(timeOut3)
+   }
+  },[])
+  
   return (
-    <div className="loader_container">
-      <Ring color="#BAFF39" width="100px" height="100px" duration="3s" />
-    </div>
-  );
-};
+    
+      <div className='loader_container'>
+        <Spin  color="#BAFF39" className='spin'  duration="10s"/>
+        <p className='text'>{loading}</p>
+       </div>
+  
+  )
+}
 
 export default Loader;
